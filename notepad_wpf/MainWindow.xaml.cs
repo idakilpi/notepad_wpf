d81e14af05b479dc669b06e24afef9fe85923ad4
Ida Yliluoma <Ida.Yliluoma@student.careeria.fi>
2024-02-15 12:42:00 +0200

    Toiminnallisuudet

--- notepad_wpf/MainWindow.xaml.cs
+++ notepad_wpf/MainWindow.xaml.cs
@@ -1,13 +1,10 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Printing;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
 using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace notepad_wpf
 {
@@ -35,6 +32,42 @@ namespace notepad_wpf
             Window1 fontSettingsWindow = new Window1();
             fontSettingsWindow.Owner = this;
             fontSettingsWindow.ShowDialog();
+        }
+        private void AvaaMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string fileName = openFileDialog.FileName;
+                textBox.Text = File.ReadAllText(fileName);
+            }
+        }
+
+        private void TallennaNimellaMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                string fileName = saveFileDialog.FileName;
+                File.WriteAllText(fileName, textBox.Text);
+            }
+        }
+
+        private void TulostaMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            PrintDialog printDialog = new PrintDialog();
+            if (printDialog.ShowDialog() == true)
+            {
+                FlowDocument document = new FlowDocument(new Paragraph(new Run(textBox.Text)));
+                printDialog.PrintDocument(((IDocumentPaginatorSource)document).DocumentPaginator, "Printing from WPF App");
+            }
+        }
+        private void MainWindow_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.Text))
+            {
+                textBox.Text += (string)e.Data.GetData(DataFormats.Text);
+            }
         }
     }
 }
